50159209bb4ad8f411467562f058bd58278f5c45
Intern <intern@vhcompanions.org>
2021-09-08 18:25:56 +0200

    Initial commit

---
+++ src/components/navBar/navBar.js
@@ -0,0 +1,49 @@
+import React, { Component, useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar(props) {
+    const [currentTab, setCurrentTab] = useState(() => {
+      switch (window.location.pathname) {
+        case "" || "/":
+          return "/";
+        case "/beer":
+          return "/chart";
+        default:
+          return window.location.pathname;
+      }
+    });
+
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="/" className="nav-link">
+                        Homepage
+                    </a>
+                </li>
+                <li>
+                    <a href="/chart" className="nav-link">
+                        My Charts
+                    </a>
+                </li>
+                <li>
+                    <a href="/songs" className="nav-link">
+                        Songs List
+                    </a>
+                </li>
+                <li>
+                    <a href="/blog" className="nav-link">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="/blog" className="nav-link">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/pages/chart.js
@@ -0,0 +1,12 @@
+function Chart()
+{
+    return(
+        <div className="site-section">
+            <div className="container">
+            
+            </div>
+        </div>
+    )
+}
+
+export default Chart;
