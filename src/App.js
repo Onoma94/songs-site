daf63fd835742b3959d191e76a9fca266f11a178
Intern <intern@vhcompanions.org>
2021-09-11 17:40:05 +0200

    Style corrections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Router from "./services/router";
 import './App.css';
 import './style.css';
@@ -6,17 +6,18 @@ import './style.css';
 import TopBar from "./components/topBar/topBar";
 import NavBar from "./components/navBar/navBar";
 import Footer from "./components/footer/footer";
-
+//'url(/footer_lodyas.png)'
+//"https://star-wars.pl/Layout/Bastion4/MCQ-forum.jpg")
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <body>
+      <div className="wrapper" style={{backgroundImage: 'require(url(/footer_lodyas.png)', backgroundSize: '100%', backgroundRepeat: 'repeat'}}>
         <TopBar />
         <NavBar />
         <Router />
         <Footer />
-      </header>
-    </div>
+      </div>
+    </body>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ function Home()
     return(
         <div className="site-section">
             <div className="container">
-            
+            <br /><br /> hello world <br /><br />
             </div>
         </div>
     )
